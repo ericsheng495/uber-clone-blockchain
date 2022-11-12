4a2bd71240b01e5e38969988516c7996a8a8ee62
ericsheng495 <eric.sheng495@gmail.com>
2022-11-12 01:18:40 -0500

    adding navbar styling and icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import profilePic from "../images/pfp.webp";
 
 const style = {
-  wrapper:
-    "h-16 w-full bg-black text-white flex md:justify-around items-center px-60 z-20",
+  wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
+  leftMenu: `flex gap-3`,
+  logo: `text-3xl text-white flex cursor-pointer mr-16`,
+  menuItem: `text-lg text-white font-medium flex items-center mx-4 cursor-pointer`,
+  rightMenu: `flex gap-3 items-center`,
+  userImageContainer: `mr-2`,
+  userImage: `h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`,
+  loginButton: `flex items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1`,
+  loginText: `ml-2`,
 };
+
+const currentAccount = "";
 
 const Navbar = () => {
   return (
@@ -17,7 +28,10 @@ const Navbar = () => {
       <div className={style.rightMenu}>
         <div className={style.menuItem}>Help</div>
         <div className={style.menuItem}>Eric</div>
-        <div className={style.userImageContainer}></div>
+        <div className={style.userImageContainer}>
+          <Image src={profilePic} width={40} height={40} alt={""} />
+        </div>
+        {currentAccount ? <div>0x0000.0000</div> : <div>please log in</div>}
       </div>
     </div>
   );
